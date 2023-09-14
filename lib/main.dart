a595c2101282d5615114166bbb6c535ed5a877c7
jakhongir0614 <jokhisgreat@gmail.com>
2023-09-14 23:53:11 +0500

    some argumants are added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:boss_blog/screens/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'constants.dart';
@@ -12,22 +13,8 @@ class BossBlog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: Text(
-              'BossBlog',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w400,
-                fontFamily: 'Playfair'
-              ),
-            ),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: true,
+      home: const OnboardingScreen(),
     );
   }
 }
--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,2 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  List<PageViewModel> getPages() {
+    return [ PageViewModel(image: Image.asset('assets/onboarding_screen/1.png',width: 400,),
+      title:'Qulay va ishonchli',
+      body: 'Endi BossBlog ilovasi orqali hoxlagan joyingizdan turib ishonchli mualliflar qoldirgan hikoyalari va bloglari blan tanisha olasiz.',
+      footer: Text('bossblog'
+      ),decoration: PageDecoration()
+    ),PageViewModel(image: Image.asset('assets/onboarding_screen/6263767_3236267.jpg'),
+      title:'Qulay va ishonchli',
+      body: 'Endi BossBlog ilovasi orqali hoxlagan joyingizdan turib ishonchli mualliflar qoldirgan hikoyalari va bloglari blan tanisha olasiz.',
+      footer: Text('bossblog'
+      ),
+    ),PageViewModel(image: Image.asset('assets/onboarding_screen/6263767_3236267.jpg'),
+      title:'Qulay va ishonchli',
+      body: 'Endi BossBlog ilovasi orqali hoxlagan joyingizdan turib ishonchli mualliflar qoldirgan hikoyalari va bloglari blan tanisha olasiz.',
+      footer: Text('bossblog'
+      ),
+    ),
+    ];
+  }
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Center(
+      child: IntroductionScreen(done: Text('Done'),onDone: (){},pages: getPages(),globalBackgroundColor: Colors.white,showNextButton: false ,
+
+      ),
+    ),);
+  }
+}
